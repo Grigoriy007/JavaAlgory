1ea0463267ebd9458b870fd690ea5078cbadf3f3
Grigoriy Legal <grigoriylegal@yandex.ru>
2023-06-22 17:27:29 +0300

    Вторая домашка

--- src/main/java/org/example/HW2/LinList.java
+++ src/main/java/org/example/HW2/LinList.java
@@ -4,57 +4,52 @@ public class LinList {
     private Node head;
     private Node tail;
 
-    public void addElements(int value){
+    public void addElements(int value) {
         Node node = new Node();
         node.value = value;
-        if (head == null){
+        if (head == null) {
             head = node;
             tail = node;
-        }
-        else{
+        } else {
             tail.next = node;
             node.previous = tail;
             tail = node;
         }
     }
 
-    public void revert(){
+    public void revert() {
         Node currentNode = head;
 
-        while (currentNode != null){
+        while (currentNode != null) {
             Node next = currentNode.next;
             Node previous = currentNode.previous;
             currentNode.next = previous;
             currentNode.previous = next;
 
-            if (previous == null){
+            if (previous == null) {
                 tail = currentNode;
             }
-            if (next == null){
+            if (next == null) {
                 head = currentNode;
             }
             currentNode = next;
         }
+    }
 
 
-
-
-        }
-
-
-    private class Node{
+    private class Node {
         private int value;
         private Node next;
         private Node previous;
     }
 
-    public void print(){
-        if (head == null){
+    public void print() {
+        if (head == null) {
             System.out.println("Список пуст");
             return;
         }
         LinList.Node node = head;
-        while (node != null){
+        while (node != null) {
             System.out.print(node.value + " -> ");
             node = node.next;
         }
