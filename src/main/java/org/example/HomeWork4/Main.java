ca1d6c4644ffc57304a62ee6602ec70cc7e563c9
Grigoriy Legal <grigoriylegal@yandex.ru>
2023-06-23 15:56:51 +0300

    Четвертая домашка

--- src/main/java/org/example/HomeWork4/Main.java
+++ src/main/java/org/example/HomeWork4/Main.java
@@ -1,2 +1,15 @@
-package org.example.HomeWork4;public class Main {
+package org.example.HomeWork4;
+
+public class Main {
+    public static void main(String[] args) {
+        TreeRedBlack tree = new TreeRedBlack();
+        tree.add(5);
+        tree.add(4);
+        tree.add(12);
+        tree.add(76);
+        tree.add(32);
+        tree.add(11);
+        tree.add(77);
+
+    }
 }
